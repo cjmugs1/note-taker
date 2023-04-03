56d04956d3e321b352192d92ebfefc314b216bb9
Connor Mulligan <connor26m@gmail.com>
2023-04-02 22:46:43 -0600

    trying to fix heroku

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const routes = require('./routes')
+const path = require("path");
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,6 +13,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes)
 
 app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+)
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
